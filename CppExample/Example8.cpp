4274786773242863a025a2d0c45d1c063ae6ba03
sanggi <sangiki07@gmail.com>
2019-07-14 13:35:02 +0900

    [기초-입출력]정수 한 개 입력받아 그대로 출력하기

---
+++ CppExample/Example8.cpp
@@ -0,0 +1,28 @@
+// 코드업 기초 100제 - [기초-입출력] 정수 한 개 입력받아 그대로 출력하기
+/*
+문제 : 
+키보드로 입력한 정수값을 그대로  출력하는 프로그램을 작성해보자.
+
+입력
+정수값 한 개가 입력된다.
+
+출력
+입력된 정수를 그대로 출력한다.
+
+
+ */
+
+#include<stdio.h>
+
+
+using namespace std;
+
+
+int main()
+{
+    int iInputNum;
+    scanf("%d",&iInputNum);
+    printf("%d",iInputNum);
+    
+    return 0;
+};
