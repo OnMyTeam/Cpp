be3f8e46becf9e26bca03acce0f32fd44af434a7
sanggi <sangiki07@gmail.com>
2019-07-14 17:22:00 +0900

    [기초-출력]출력하기 05

---
+++ CppExample/Example05.cpp
@@ -0,0 +1,26 @@
+// 코드업 기초 100제 - [기초-출력] 출력하기 5
+/*
+문제 : 
+이번에는 큰 따옴표(") (Double Quotation mark)를 문장에 넣어 출력해야 한다.
+
+다음과 같은 문장을 출력하시오.
+
+
+"Hello World"
+
+(주의 : 큰 따옴표(")도 함께 출력되어야 한다.)
+
+ */
+
+#include<stdio.h>
+
+
+using namespace std;
+
+
+int main()
+{
+    printf("\"Hello World\"");
+    
+    return 0;
+}
