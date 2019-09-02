5dcb8408d86567bdc0f125395436884277552cbf
sanggi <sangiki07@gmail.com>
2019-09-02 17:03:59 +0900

    정렬 알고리즘개요 및 선택정렬(Selection Sort)

--- CppBasicAlgorithm/Example01.cpp
+++ CppBasicAlgorithm/Example01.cpp
@@ -1,1 +1,36 @@
-dfdf
+#include <stdio.h>
+#include <array>
+
+using namespace std;
+int main(void){
+    
+    int irArray[7] = {1, 8, 5, 10, 2, 7, 9};
+    int ilength = sizeof(irArray)/sizeof(irArray[0]);
+     
+    int i, j, min, index, temp;
+
+    for(i=0; i<ilength;i++){
+        min = 9999;
+        for(j=i; j<ilength; j++){
+            
+            if(min > irArray[j]){
+                
+                min = irArray[j];
+                index = j;
+
+            }
+
+        }
+        temp = irArray[i];
+        irArray[i] = min;
+        irArray[index] = temp;
+
+        
+    }
+    for(int i=0; i<ilength; i++){
+        printf("%d\n", irArray[i]);
+    }
+    
+
+    return 0;
+}
